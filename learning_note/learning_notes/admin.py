e5b5d27ecb392fdc8073463e39752769d876e5af
coohx <huangxin2028@gmail.com>
2017-02-08 22:21:26 +0800

    add entry

--- learning_note/learning_notes/admin.py
+++ learning_note/learning_notes/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-# 导入我们要注册的模型Topic
-from learning_notes.models import Topic
+# 导入我们要注册的模型
+from learning_notes.models import Topic, Entry
 
 # Django通过管理网站管理我们的模型
 admin.site.register(Topic)
+admin.site.register(Entry)
 
--- learning_note/learning_notes/models.py
+++ learning_note/learning_notes/models.py
@@ -11,3 +11,23 @@ class Topic(models.Model):
         # Django调用方法__str__()返回属性text来表示关于“主题”的信息
         return self.text
 
+class Entry(models.Model):
+    """学习到的有关某个主题的具体知识"""
+    # 数据库外键，引用了表Topic中的一条记录，关联到特定的主题
+    topic = models.ForeignKey(Topic)
+    # TextField()字段不需要长度限制
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """在Entry类中嵌套类Meta"""
+        # 特殊属性，Django在需要时使用Entries来表示多个条目
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        # 对字符串进行切片，只显示前50个字符，外加省略号
+        if len(self.text) >= 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
