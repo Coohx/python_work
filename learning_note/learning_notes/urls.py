c89aa71bf58ebebbc27830867fb8d8ac0b442868
Coohx <huangxin2028@gmail.com>
2017-03-03 21:55:31 +0800

    update

--- learning_note/learning_notes/urls.py
+++ learning_note/learning_notes/urls.py
@@ -8,7 +8,10 @@ from . import views
 urlpatterns = [
     # 项目主页的一个URL模式
     # Python忽略项目的基础URL,r'^$'直接匹配到基础URL;当请求的URL不与任何URL模式匹配时，Djang返回一个错误页面
-    # Django会调用视图函数views.index
+    # Django会调用视图函数views.index()
     # name实参指定了这个URL模式的名称，以便能够在代码的其他地方引用它(index)
     url(r'^$', views.index, name='index'),
+
+    # “学习笔记”主题的URL模式
+    url(r'^topics/&', views.topics, name='topics'),
 ]
--- learning_note/learning_notes/views.py
+++ learning_note/learning_notes/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from .models import Topic
 # Create your views here.
+
+# 服务器请求对象request
 def index(request):
     """学习笔记的主页视图"""
     # 函数render()根据视图提供的数据渲染响应
     return render(request, 'learning_notes/index.html')
+
+def topics(request):
+    """显示所有的主题"""
+    # 查询数据库，返回查询集(列表)，按添加时间排序
+    topics = Topic.objects.order_by('date_added')
+    # 模板将使用字典数据用于生成网页
+    contex = {'topics': topics}
+    return render(request, 'learning_notes/topics.html', contex)
 
