91a8c74950e41ae96ceef70b5e251825138d66b5
Coohx <huangxin2028@gmail.com>
2017-04-01 21:19:37 +0800

    data visualization

---
+++ py_data_visualization/README.md
@@ -0,0 +1,5 @@
+### Python 数据可视化
+
+- matplotlib数学绘图库[matplotlib](http://matplotlib.org)
+
+- Pygal:[Python绘图工具](http://pygal.org)
---
+++ py_data_visualization/mpl_squares.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+plt.plot(squares)
+plt.show()
+
